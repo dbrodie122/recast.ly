475ce54eda08e0c1a207dba1e0f145e80078a600
David Brodie <david.brodie122@gmail.com>
2017-02-23 17:15:56 -0800

    Implement up to first click handler - inclomplete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,36 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+      playing: exampleVideoData[0]
+    };
+  }
+
+  onTitleClick () {
+    console.log("OMG")
+    this.setState({playing: });
+  }
+  //We need to grab the exampleVideoData that is associated with the element that was clicked.
+	
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList  videos={exampleVideoData} onTitleClick={this.onTitleClick}/>
+        </div>
+      </div>
+    );
+  }   
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,13 @@
-var VideoList = () => (
+//DB:tests indicate this is being set up properly.  But, since nothing is rendering, it seems that it is not being passed the correct data.  deleting the exampleVideoData from app.jsx does nothing to the tests or to what is being rendered on the page.
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+    {props.videos.map(video => 
+      <VideoListEntry  video={video} onTitleClick={props.onTitleClick} />
+      )}
   </div>
 );
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
